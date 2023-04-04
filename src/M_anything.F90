4c855ee56164e7f12be43d0b55498e5e8815625a
John S. Urban <urbanjost@comcast.net>
2023-04-04 19:22:04 -0400

    kludge around ifx bug

--- src/M_anything.F90
+++ src/M_anything.F90
@@ -421,11 +421,8 @@ character(len=1),allocatable :: chars(:)
 #endif
     type is (logical);              chars=transfer(anything,chars)
     class default
-!#ifdef __INTEL_LLVM_COMPILER
-!      stop 'crud. anything_to_bytes_arr(1) does not know about this type'
-!#else
+      !stop 'crud. anything_to_bytes_arr(1) does not know about this type'
       chars=transfer(anything,chars) ! should work for everything, does not with some compilers
-!#endif
    end select
 
 end function anything_to_bytes_arr
